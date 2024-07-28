d065b63a09c09e20312c0eea2e31b60d8ca68f61
TomerBu <tomer.bu@gmail.com>
2024-07-28 21:49:18 +0300

    lec5

--- ApisModuleLec3/ApisModuleLec3/Controllers/CardsController.cs
+++ ApisModuleLec3/ApisModuleLec3/Controllers/CardsController.cs
@@ -9,7 +9,7 @@ namespace ApisModuleLec3.Controllers
 {
 	[Route("api/[controller]")]
 	[ApiController]
-	public class CardsController (IRepository<Card> cardsRepo): ControllerBase
+	public class CardsController(IRepository<Card> cardsRepo) : ControllerBase
 	{
 		[HttpPost]
 		public async Task<IActionResult> Post([FromBody] CardAddRequest dto)
@@ -18,7 +18,7 @@ namespace ApisModuleLec3.Controllers
 			{
 				return BadRequest("BadBad");
 			}
-			var card = dto.ToCard("TheUserId");	
+			var card = dto.ToCard("TheUserId");
 			var result = await cardsRepo.AddAsync(card);
 
 			return CreatedAtAction(nameof(Get), new { id = result.Id }, result);
@@ -33,7 +33,7 @@ namespace ApisModuleLec3.Controllers
 
 
 		[HttpGet("{id}")]
-		public async Task<IActionResult> Get([FromRoute]string id)
+		public async Task<IActionResult> Get([FromRoute] string id)
 		{
 			var result = await cardsRepo.GetByIdAsync(id);
 			if (result == null)

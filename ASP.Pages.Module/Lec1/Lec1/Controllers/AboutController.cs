a8654f0c718f337e3e7bb0e2977000f206651bad
tomerbu <Tomer.bu@gmail.com>
2024-08-11 20:48:37 +0300

    lec1

---
+++ ASP.Pages.Module/Lec1/Lec1/Controllers/AboutController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lec1.Controllers
+{
+	public class AboutController(ILogger<AboutController> logger) : Controller
+	{
+		public IActionResult Index(int? id)
+		{
+			logger.LogInformation(id is null ? "No id" : $"id: {id}");
+			return View();
+		}
+
+		//    localhost:3333/About/Social
+		public IActionResult Social()
+		{
+			return View();
+		}
+	}
+}
---
+++ ASP.Pages.Module/Lec1/Lec1/Controllers/BooksController.cs
@@ -0,0 +1,25 @@
+﻿using Lec1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lec1.Controllers
+{
+    public class BooksController : Controller
+    {
+        private static List<BookViewModel> books = [
+            new BookViewModel() {
+                Id = 1,
+                Title = "Harry potter 1",
+                Description = "The only book :-)"
+            },
+            new BookViewModel() {
+                Id = 2,
+                Title = "The Godfather",
+                Description = "The only other book :-)"
+            }
+        ];
+        public IActionResult Index()
+        {
+            return View(books);
+        }
+    }
+}

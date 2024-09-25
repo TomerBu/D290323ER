1b0f0e3e69c8af7c190dbf66a3067a0d5d1cab9e
tomerbu <Tomer.bu@gmail.com>
2024-09-25 18:47:33 +0300

    feature: Lec6 Update Navbar styling and dark mode functionality

--- Final.Module.Summary.And.Cloud.Deployment/frontend/src/components/Navbar.tsx
+++ Final.Module.Summary.And.Cloud.Deployment/frontend/src/components/Navbar.tsx
@@ -1,43 +1,45 @@
 import { BsGithub } from "react-icons/bs";
 import { SiHomebridge } from "react-icons/si";
 import { NavLink } from "react-router-dom";
-import './Navbar.scss';
+import "./Navbar.scss";
 import { useContext } from "react";
 import { DarkModeContext } from "../contexts/DarkModeContext";
 
 //rafce
 const Navbar = () => {
-
-  const {darkMode, toggle} = useContext(DarkModeContext);
+  const { darkMode, toggle } = useContext(DarkModeContext);
   return (
-    <nav id="app-nav" className="shadow-2xl p-8 flex gap-3 bg-fuchsia-50 text-fuchsia-900 dark:bg-fuchsia-900 dark:text-fuchsia-50">
-      
-      <button onClick={toggle} className="rounded-lg p-2">
-         {darkMode ? "🌞" : "🌚"}
-      </button>
-      
+    <nav
+      id="app-nav"
+      className="shadow-2xl p-8 flex gap-3 bg-fuchsia-50 text-fuchsia-900 dark:bg-fuchsia-900 dark:text-fuchsia-50"
+    >
       <NavLink className="rounded-lg p-2" to="/">
         <SiHomebridge aria-description="Home" />
       </NavLink>
 
+      <NavLink className="rounded-lg p-2" to="/about">
+        About
+      </NavLink>
+      <NavLink className="rounded-lg p-2" to="/products">
+        Products
+      </NavLink>
       <div className="flex-1"></div>
 
       <div className="hidden sm:flex items-center">
-        <NavLink className="rounded-lg p-2" to="/about">
-          About
-        </NavLink>
-        <NavLink className="rounded-lg p-2" to="/products">
-          Products
-        </NavLink>
         <NavLink className="rounded-lg p-2" to="/login">
           Login
         </NavLink>
         <NavLink className="rounded-lg p-2" to="/register">
           Register
         </NavLink>
+
         <a href="https://github.com/TomerBu/D290323ER" className="px-2">
           <BsGithub aria-description="Github" />
         </a>
+
+        <button onClick={toggle} className="rounded-lg p-2">
+          {darkMode ? "🌞" : "🌚"}
+        </button>
       </div>
     </nav>
   );
--- Final.Module.Summary.And.Cloud.Deployment/frontend/src/contexts/DarkModeContext.tsx
+++ Final.Module.Summary.And.Cloud.Deployment/frontend/src/contexts/DarkModeContext.tsx
@@ -3,7 +3,7 @@
 //state variable: isDark
 //function to toggle dark mode
 
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const initialValues = {
   darkMode: false,
@@ -14,12 +14,27 @@ const DarkModeContext = createContext(initialValues);
 
 function DarkModeProvider({ children }) {
 
-  //state variables:  
+  useEffect(() => {
+    const mode = localStorage.getItem("darkMode");
+    if (mode === "dark") {
+      setDarkMode(true);
+      document.body.classList.toggle("dark");
+    }
+  }, []);
+ 
   const [darkMode, setDarkMode] = useState(false);
 
   //functions:
   function toggle() {
+    //calculate the new state as a string: "dark" or "light":
+    const newMode = !darkMode ? "dark" : "light";
+
+    //save the new state to local storage:
+    localStorage.setItem("darkMode", newMode);
+
     setDarkMode((prev) => !prev);
+
+    document.body.classList.toggle("dark");
   }
 
   return (
@@ -32,6 +47,3 @@ function DarkModeProvider({ children }) {
 export { DarkModeProvider, DarkModeContext };
 
 //useContext(DarkModeContext) to access the context in a component
-
-
-
